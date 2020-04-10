5caddd0ad8c8c84d55d2204cb69600ba41d31dba
ayumu <ayumuban@outlook.com>
2020-04-10 22:30:04 +0900

    :sparkles: Complete advanced setup (Chap3)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+# *** minitest reporters ***
+# ref: https://railstutorial.jp/chapters/static_pages?version=5.1#sec-minitest_reporters
+require "minitest/reporters"
+Minitest::Reporters.use!
+# **************************
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
